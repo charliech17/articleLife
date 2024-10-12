19d4d8da9ee6dccd2b64a05ece4942376d57f979
Josh <kcchang1994@gmail.com>
2024-10-12 21:54:46 +0800

    Fix: 讓分散的i18n檔案打包到en.json及zh-tw.json檔案中，讓i18n可以正常運作。

---
+++ esbuild/merge-json-plugin.ts
@@ -0,0 +1,42 @@
+import { PluginBuild } from 'esbuild';
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+
+const mergeJsonPlugin = {
+  name: 'merge-json',
+  setup(build: PluginBuild) {
+    build.onStart(() => {
+      // Define the JSON patterns and output files
+      const jsonGroups = [
+        { pattern: './src/i18n/en/*.json', outputFile: './src/i18n/en.json' },
+        { pattern: './src/i18n/zh-tw/*.json', outputFile: './src/i18n/zh-tw.json' },
+        // 其他需要合併的語言文件
+      ];
+
+      jsonGroups.forEach(group => {
+        // Find all JSON files that match the pattern
+        const files = glob.sync(group.pattern);
+        const mergedData = {};
+
+        files.forEach((file: any) => {
+          console.log('file' + JSON.stringify(file));
+          const content = JSON.parse(fs.readFileSync(file, 'utf8'));
+          Object.assign(mergedData, content);
+        });
+
+        // Ensure the output directory exists
+        const outputDir = path.dirname(group.outputFile);
+        if (!fs.existsSync(outputDir)) {
+          fs.mkdirSync(outputDir, { recursive: true }); // Create the directory if it doesn't exist
+        }
+
+        // Write the merged JSON to the output file
+        fs.writeFileSync(group.outputFile, JSON.stringify(mergedData, null, 2), 'utf8');
+        console.log(`Merged JSON files for ${group.outputFile}`);
+      });
+    });
+  },
+};
+
+module.exports = mergeJsonPlugin;
--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateDirective } from '../../shared/language';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TranslateDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
