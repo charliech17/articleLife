c93189c90999d1ccbc0a2a722739a20c1369d5d1
Josh <kcchang1994@gmail.com>
2024-10-13 22:39:55 +0800

    Add: poc install and use 'ckeditor5'

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { afterNextRender, ChangeDetectorRef, Component } from '@angular/core';
 import { TranslateDirective } from '../../shared/language';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { ClassicEditor } from 'ckeditor5';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateDirective],
+  imports: [TranslateDirective, CKEditorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  editor: typeof ClassicEditor | null = null;
+  config: typeof ClassicEditor.defaultConfig | null = null;
+
+  constructor(private cdr: ChangeDetectorRef) {
+    afterNextRender(() => {
+      this.loadCkEditor();
+    });
+  }
+
+  async loadCkEditor() {
+    const { Bold, Essentials, Italic, Mention, Paragraph, Undo, ClassicEditor, Heading } = await import('ckeditor5');
+    this.config = {
+      toolbar: ['undo', 'redo', '|', 'bold', 'italic', 'Heading'],
+      plugins: [Bold, Essentials, Italic, Mention, Paragraph, Undo, Heading],
+      heading: {
+        options: [
+          { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+          { model: 'heading1', view: 'h2', title: '區塊title', class: 'ck-heading_heading1' },
+        ],
+      },
+    };
+    this.editor = ClassicEditor;
+    this.cdr.detectChanges();
+  }
+}
