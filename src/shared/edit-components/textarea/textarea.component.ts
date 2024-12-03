fc370d2e12a1ee30b4e4df2d5c17da56ba0f9606
Josh <kcchang1994@gmail.com>
2024-12-03 22:45:19 +0800

    Add: 新增textarea.component.ts，在輸入事件產生後，debounce將資料傳至父元件，以避免change事件需要離開輸入框才能觸發。初始化元件時就執行resize，避免有些文字沒有顯示。

--- src/shared/edit-components/textarea/textarea.component.ts
+++ src/shared/edit-components/textarea/textarea.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, ElementRef, input, OnInit, output, signal, ViewChild } from '@angular/core';
+import { Component, computed, effect, ElementRef, input, OnInit, output, signal, ViewChild } from '@angular/core';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { EditTitleComponent, IEditTitleInput } from '../edit-title/edit-title.component';
 
@@ -15,25 +15,45 @@ export class TextareaComponent implements OnInit {
     return `cus-edit_${this.$iptTextarea().headerType}-title`;
   });
   optUpdateText = output<string>();
+  timeOutId: ReturnType<typeof setTimeout> | undefined = undefined;
 
   @ViewChild('textarea') textarea!: ElementRef<HTMLTextAreaElement>;
   textControl = new FormControl('', { nonNullable: true });
+
+  constructor() {
+    this.onInputChangeResizeTextarea();
+  }
 
   ngOnInit(): void {
     this.textControl.setValue(this.$iptTextarea().initText);
   }
 
-  handleInputEvent(): void {
-    this.resizeTextarea();
+  private onInputChangeResizeTextarea(): void {
+    effect(() => {
+      this.$iptTextarea();
+      this.resizeTextarea();
+    });
   }
 
-  handleChangeEvent(): void {
-    this.emitToParent(this.textControl.value);
+  handleInputEvent(): void {
+    this.resizeTextarea();
+    this.debounceTextInput();
+  }
+
+  private debounceTextInput() {
+    clearTimeout(this.timeOutId);
+    this.timeOutId = setTimeout(() => {
+      this.emitToParent(this.textControl.value);
+    });
   }
 
   resizeTextarea(): void {
     this.textarea.nativeElement.style.height = 'auto';
     this.textarea.nativeElement.style.height = `${this.textarea.nativeElement.scrollHeight}px`;
+  }
+
+  handleChangeEvent(): void {
+    this.emitToParent(this.textControl.value);
   }
 
   emitToParent(newText: string): void {
