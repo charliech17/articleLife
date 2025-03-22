d1602374c5af0a416161e9fc19ded25797c49434
Josh <kcchang1994@gmail.com>
2025-03-22 19:17:44 +0800

    Add: frontend CSP config

--- server.ts
+++ server.ts
@@ -5,6 +5,8 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 import { environment } from './src/shared/env/env';
+import { Request, Response } from 'express-serve-static-core';
+import QueryString from 'qs';
 
 const basePath = environment.baseHref || '/';
 // The Express app is exported so that it can be used by serverless Functions.
@@ -33,6 +35,7 @@ export function app(): express.Express {
   // All regular routes use the Angular engine
   server.get(`${basePath}/*`, (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
+    setResHeaders(req, res);
 
     commonEngine
       .render({
@@ -47,6 +50,30 @@ export function app(): express.Express {
   });
 
   return server;
+}
+
+function setResHeaders(
+  req: Request<{}, any, any, QueryString.ParsedQs, Record<string, any>>,
+  res: Response<any, Record<string, any>, number>,
+) {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  res.setHeader('Permissions-Policy', 'camera=(), geolocation=(), microphone=(), payment=() ');
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self' 'unsafe-inline'; frame-src 'self' https://www.facebook.com/ https://social-plugins.line.me/ https://www.youtube.com/ https://josh-lifesharing.com; img-src * data: blob: 'unsafe-inline'; connect-src http: https:; style-src 'self' 'unsafe-inline'; frame-ancestors 'self' https://www.facebook.com/ https://social-plugins.line.me/ https://www.youtube.com/;object-src 'none'",
+  );
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+  res.setHeader('Access-Control-Allow-Headers', 'X-XSRF-TOKEN Content-Type');
+
+  // Set ip address headerss
+  const xRealIp = req.headers['x-real-ip'];
+  const xForwardedFor = req.headers['x-forwarded-for'];
+  res.setHeader('X-Real-IP', xRealIp || '');
+  res.setHeader('X-Forwarded-For', xForwardedFor || '');
 }
 
 function run(): void {
